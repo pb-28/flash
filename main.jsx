b4c7d00fd641d6955a9f5234f12d59baec6ed100
Zosez <2kashyap.adhikari@gmail.com>
2025-11-25 21:17:11 +0545

    html and another file

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FlashPickleballWebsite from "./flash-pickleball-site.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FlashPickleballWebsite />
+  </React.StrictMode>
+);
